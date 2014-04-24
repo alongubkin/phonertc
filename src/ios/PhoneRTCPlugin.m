c136b45259fb6acaf22636901b8e675901103994
Alon Gubkin <alon.gubkin@gmail.com>
2014-04-24 16:23:02 +0300

    fix(ios): isInitiator is not set

--- src/ios/PhoneRTCPlugin.m
+++ src/ios/PhoneRTCPlugin.m
@@ -28,6 +28,7 @@
                                     password: turnPassword];
         
         self.webRTC = [[PhoneRTCDelegate alloc] init];
+        self.webRTC.isInitator = isInitator;
         
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sendMessage:) name:@"SendMessage" object:nil];
         [self.webRTC onICEServers:@[stunServer, turnServer]];
