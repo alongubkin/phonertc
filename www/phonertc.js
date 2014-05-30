d754c1646b4ef308d06a7d55d2f9a822fc4a6b5e
Joseph Frazier <joseph@onsip.com>
2014-05-30 17:29:46 -0400

    Update phonertc.js

--- www/phonertc.js
+++ www/phonertc.js
@@ -1,34 +1,36 @@
-var phonertc = { 
-	call: function (options) {
-		cordova.exec(
-			function (data) { 
-				if (data.type === '__answered' && options.answerCallback) {
-					options.answerCallback();
-				} else if (data.type === '__disconnected' && options.disconnectCallback) {
-					options.disconnectCallback();
-				} else {
-					options.sendMessageCallback(data);
-				}
-			},
-			null,
-			'PhoneRTCPlugin',
-			'call',
-			[options.isInitator, options.turn.host, options.turn.username, options.turn.password]);
-	},
-	receiveMessage: function (data) { 
-		cordova.exec(
-			null, 
-			null,
-			'PhoneRTCPlugin',
-			'receiveMessage',
-			[JSON.stringify(data)]);
-	},
-	disconnect: function () {
-		cordova.exec(
-			null, 
-			null,
-			'PhoneRTCPlugin',
-			'disconnect',
-			[]);
-	}
+var exec = require('cordova/exec');
+
+exports.call = function (options) {
+  exec(
+    function (data) {
+      if (data.type === '__answered' && options.answerCallback) {
+        options.answerCallback();
+      } else if (data.type === '__disconnected' && options.disconnectCallback) {
+        options.disconnectCallback();
+      } else {
+        options.sendMessageCallback(data);
+      }
+    },
+    null,
+    'PhoneRTCPlugin',
+    'call',
+    [options.isInitator, options.turn.host, options.turn.username, options.turn.password]);
+};
+
+exports.receiveMessage = function (data) {
+  exec(
+    null,
+    null,
+    'PhoneRTCPlugin',
+    'receiveMessage',
+    [JSON.stringify(data)]);
+};
+
+exports.disconnect = function () {
+  exec(
+    null,
+    null,
+    'PhoneRTCPlugin',
+    'disconnect',
+    []);
 };
