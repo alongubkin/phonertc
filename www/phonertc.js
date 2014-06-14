71f176912ea2c2386b5348f21e47a94af4b98f67
Eric Green <eric.douglas.green@gmail.com>
2014-06-14 10:50:32 -0400

    Add javascript to pull video elements off page and pass to native iOS

--- www/phonertc.js
+++ www/phonertc.js
@@ -1,6 +1,38 @@
 var exec = require('cordova/exec');
 
+var videoElements;
+
+document.addEventListener("touchmove", function () {
+  // This function should listen for scrolling and update the position of the elements to cordova exec
+  if (videoElements) {
+    // Update Video Element positioning
+  }
+});
+
 exports.call = function (options) {
+  // options should contain a video option if video is enabled
+  // sets the initial video options a dom listener needs to be added to watch for movements.
+  var video;
+  if (options.video) {
+    videoElements = video = {};
+    videoElements.localVideo = options.video.localVideo;
+    videoElements.remoteVideo = options.video.remoteVideo;
+    video.localVideo = {
+      // get these values by doing a lookup on the dom
+      x : videoElements.localVideo.offsetLeft,
+      y : videoElements.localVideo.offsetTop,
+      width : videoElements.localVideo.offsetWidth,
+      height: videoElements.localVideo.offsetHeight
+    },
+    video.remoteVideo = {
+      // get these values by doing a lookup on the dom
+      x : videoElements.remoteVideo.offsetLeft,
+      y : videoElements.remoteVideo.offsetTop,
+      width : videoElements.remoteVideo.offsetWidth,
+      height : videoElements.remoteVideo.offsetHeight
+    }
+  }
+
   exec(
     function (data) {
       if (data.type === '__answered' && options.answerCallback) {
@@ -14,7 +46,7 @@ exports.call = function (options) {
     null,
     'PhoneRTCPlugin',
     'call',
-    [options.isInitator, options.turn.host, options.turn.username, options.turn.password]);
+    [options.isInitator, options.turn.host, options.turn.username, options.turn.password, videoElements]);
 };
 
 exports.receiveMessage = function (data) {
