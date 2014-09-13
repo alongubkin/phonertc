2dbb1b3e07622729eba5e91eef42edc177789bea
Alon Gubkin <alon.gubkin@gmail.com>
2014-09-13 17:44:01 +0300

    basic contacts state

--- demo/client/app/scripts/app.js
+++ demo/client/app/scripts/app.js
@@ -1,21 +1,33 @@
 'use strict';
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('Client', ['ionic', 'config'])
+angular.module('phonertcdemo', ['ionic', 'ui.router', 'phonertcdemo.config'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/app.html'
+      })
+      .state('app.contacts', {
+        url: '/contacts',
+        templateUrl: 'templates/contacts.html'
+      });
+
+    $urlRouterProvider.otherwise('/app/contacts');
+  })
+
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+
+      if(window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   });
-});
